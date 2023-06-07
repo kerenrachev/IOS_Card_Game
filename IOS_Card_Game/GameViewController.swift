3971bb6104d56268cdedebda5067b3cd8f626126
kerenrachev <keren1997rachev@gmail.com>
2023-06-07 20:15:45 +0300

    Second screen UI

---
+++ IOS_Card_Game/GameViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GameViewController.swift
+//  IOS_Card_Game
+//
+//  Created by Student35 on 07/06/2023.
+//
+
+import Foundation
---
+++ IOS_Card_Game/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  IOS_Card_Game
+//
+//  Created by Student35 on 07/06/2023.
+//
+
+import Foundation
