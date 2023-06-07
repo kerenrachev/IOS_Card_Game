a90ac4fc4e486a5bb6b5d5e03430d5f91843a34f
kerenrachev <keren1997rachev@gmail.com>
2023-06-07 12:18:47 +0300

    Initial Commit

---
+++ IOS_Card_Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_Card_Game
+//
+//  Created by Student35 on 07/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
