05d723d78e2f3f40eb503e3a6192558c28ca4f39
kerenrachev <keren1997rachev@gmail.com>
2023-06-07 21:37:15 +0300

    Timer

--- IOS_Card_Game/ViewGameController.swift
+++ IOS_Card_Game/ViewGameController.swift
@@ -8,17 +8,22 @@ class ViewGameController : UIViewController {
     
     private var rightScore = 0
     private var leftScore = 0
+    var countdownTimer: Timer?
+    var countdownValue = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
+        startCountdown(seconds: 5)
         setImgSource(imgObj: rightImage, imgName: "rock.jpeg")
     }
     
     @IBOutlet weak var rightImage: UIImageView!
     
     @IBOutlet weak var leftImage: UIImageView!
+    
+    
+    @IBOutlet weak var countdownLabel: UILabel!
     
     private func setImgSource(imgObj: UIImageView, imgName: String){
         if let image = UIImage(named: imgName){
@@ -28,4 +33,31 @@ class ViewGameController : UIViewController {
             print("Image was not found")
         }
     }
-}
+    
+    func startCountdown(seconds: Int) {
+        countdownValue = seconds
+        countdownLabel.text = String(countdownValue)
+            
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
+                guard let self = self else { return }
+                
+                self.countdownValue -= 1
+                
+                if self.countdownValue >= 0 {
+                    self.countdownLabel.text = String(self.countdownValue)
+                } else {
+                    self.stopCountdown()
+                    
+                    if(seconds == 5) {
+                        startCountdown(seconds: 3)
+                    }
+                    
+                }
+        }
+        
+    }
+        
+        func stopCountdown() {
+            countdownTimer?.invalidate()
+            countdownTimer = nil
+        }}
